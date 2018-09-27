2534e939c5e7d67dda07027aa8080c5728b40492
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2018-09-27 11:46:00 +0900

    Add fizzbuzz engine test

--- tests/fizzbuzz/engine_test.rb
+++ tests/fizzbuzz/engine_test.rb
@@ -1,12 +1,25 @@
-require 'minitest/autorun'
-
-Minitest::Unit.autorun
+require 'fizzbuzz/engine'
 
 class EngineTest < MiniTest::Unit::TestCase
   def setup
+    @engine = FizzBuzz::Engine.new
   end
 
-  def test_foo
-    assert_equal "foo", "bar"
+  def test_correct_output_generated_when_small_range?
+    generated = @engine.generate(1..20)
+
+    assert_equal generated.size, 20
+    assert_equal generated, [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8,
+                             "Fizz", "Buzz", 11, "Fizz", 13, 14,
+                             "Fizz Buzz", 16, 17, "Fizz", 19, "Buzz"]
+  end
+
+  def test_correct_output_when_enough_length?
+    generated = @engine.generate(1..100000)
+
+    assert_equal generated.size, 100000
+    assert_equal generated[99999], "Buzz"
+    assert_equal generated[99992], "Fizz"
+    assert_equal generated[99991], 99992
   end
 end
